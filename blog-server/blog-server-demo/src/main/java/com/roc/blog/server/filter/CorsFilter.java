5f8110bc34aefc229404da03c4f174f043a35193
peng <zhangpeng3710@gmail.com>
2023-03-21 15:49:35 +0800

    add cors solution: amend custom header

--- blog-server/blog-server-demo/src/main/java/com/roc/blog/server/filter/CorsFilter.java
+++ blog-server/blog-server-demo/src/main/java/com/roc/blog/server/filter/CorsFilter.java
@@ -5,11 +5,11 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 
 @WebFilter
@@ -22,10 +22,14 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Methods", "*");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        chain.doFilter(req, res);
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, X-Custom-Header");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            response.getOutputStream().write("Options Success".getBytes(StandardCharsets.UTF_8));
+        } else {
+            chain.doFilter(req, res);
+        }
     }
 
 }
