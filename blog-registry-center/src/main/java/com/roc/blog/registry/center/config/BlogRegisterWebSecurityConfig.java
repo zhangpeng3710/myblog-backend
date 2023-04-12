2d8b8811628902deb0f2c273cc017555ce240412
peng <zhangpeng3710@gmail.com>
2023-04-12 17:13:10 +0800

    add registry center

---
+++ blog-registry-center/src/main/java/com/roc/blog/registry/center/config/BlogRegisterWebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.roc.blog.registry.center.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description
+ * @Author: Zhang Peng
+ * @Date: 2023/4/12 16:46
+ */
+
+@EnableWebSecurity
+public class BlogRegisterWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
